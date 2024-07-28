211b63204b4cbc905e31e1db4a9ff3685c35a720
JasonS <jasons@novaleaf.com>
2024-07-28 09:42:20 -0700

    basic handshake PoC working, plus c# sdk files

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Epic-Online-Services-CSharp-Example
+# EGS-CSharp-Example
 C# "Hello, EGS!" as simply as possible. using Epic Game Services (part of Epic Online Services)
+
+I did the official Epic "hello world" last night and supremely dislike the large WPF+xaml just to show minimal usage of the actual EOS network code.
+
+Here, I take the important parts of the EOS workflow  (EGS specifically)  and ported it to a basic c# console app.   Eventually I want to add a godot example too.
+
+This is loosely based on the official Epic WPF example blog series, found here: https://dev.epicgames.com/en-US/news/introduction-to-epic-online-services-eos
---
+++ eos-console-example/Program.cs
@@ -0,0 +1,78 @@
+﻿using Epic.OnlineServices.Logging;
+using Epic.OnlineServices.Platform;
+
+namespace eos_console_example;
+
+internal class Program
+{
+
+	public static bool isShutdown = false;
+
+	static async Task Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+
+		var initOptions = new InitializeOptions()
+		{
+			ProductName = "vUltra-test1",
+			ProductVersion = "1.0.0",
+		};
+
+		var initResult = PlatformInterface.Initialize(ref initOptions);
+		Console.WriteLine($"Initialize: {initResult.ToString()}");
+
+		//add EOS logging
+		_ = LoggingInterface.SetLogLevel(LogCategory.AllCategories, LogLevel.Info);
+		_ = LoggingInterface.SetCallback((ref LogMessage message) => Console.WriteLine($"[{message.Level}] {message.Category} - {message.Message}"));
+
+		var options = new Options()
+		{
+			//get your own product id, sandbox id, client id, and client secret from
+			// https://dev.epicgames.com
+			ProductId = "a9acd901f8b04a2ebef1e170b7950acd",
+			SandboxId = "3eb2fc858abb4fc2b4816cf0f90e3a78",
+			ClientCredentials = new()
+			{
+				ClientId = "xyza7891nMxs9jY4nfYdv2IBkSyyz9E2",
+				ClientSecret = Environment.GetEnvironmentVariable("EosClientSecret"),
+			},
+			DeploymentId = "65a58e8519a74dd9b8cc1a2fc71b60e0",
+			Flags = PlatformFlags.DisableOverlay,
+			IsServer = false,
+		};
+
+		var platformInterface = PlatformInterface.Create(ref options);
+
+		if (platformInterface is null)
+		{
+			throw new Exception("Failed to create platform.");
+		}
+
+		var loopCount = 0;
+		while (true)
+		{
+
+			await Task.Delay(500);
+			Update(platformInterface, loopCount);
+
+			if (isShutdown)
+			{
+				break;
+			}
+			loopCount++;
+		}
+
+		//dispose
+		platformInterface.Release();
+		platformInterface = null;
+		var shutdownResult = PlatformInterface.Shutdown();
+		Console.WriteLine($"shudown result = {shutdownResult.ToString()}");
+	}
+
+
+	private static void Update(PlatformInterface platformInterface, int loopCount)
+	{
+		platformInterface.Tick();
+		Console.WriteLine($"Update Done: loopCount = {loopCount}");
+	}
+}
