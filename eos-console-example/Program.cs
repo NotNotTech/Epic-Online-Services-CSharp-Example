92e037662161828b62d8268af0832ae877267b31
JasonS <jasons@novaleaf.com>
2024-07-28 12:22:44 -0700

    EOS login working

--- eos-console-example/Program.cs
+++ eos-console-example/Program.cs
@@ -1,5 +1,9 @@
-﻿using Epic.OnlineServices.Logging;
+﻿using System.Diagnostics;
+using Epic.OnlineServices;
+using Epic.OnlineServices.Auth;
+using Epic.OnlineServices.Logging;
 using Epic.OnlineServices.Platform;
+using Epic.OnlineServices.UserInfo;
 
 namespace eos_console_example;
 
@@ -25,6 +29,9 @@ internal class Program
 		_ = LoggingInterface.SetLogLevel(LogCategory.AllCategories, LogLevel.Info);
 		_ = LoggingInterface.SetCallback((ref LogMessage message) => Console.WriteLine($"[{message.Level}] {message.Category} - {message.Message}"));
 
+
+
+		/********  CONNECT TO EOS  ********/
 		var options = new Options()
 		{
 			//get your own product id, sandbox id, client id, and client secret from
@@ -39,6 +46,8 @@ internal class Program
 			DeploymentId = "65a58e8519a74dd9b8cc1a2fc71b60e0",
 			Flags = PlatformFlags.DisableOverlay,
 			IsServer = false,
+
+			
 		};
 
 		var platformInterface = PlatformInterface.Create(ref options);
@@ -48,6 +57,71 @@ internal class Program
 			throw new Exception("Failed to create platform.");
 		}
 
+
+		/********  LOGIN: translated from: https://dev.epicgames.com/en-US/news/player-authentication-with-epic-account-services-eas ********/
+		//login via epic (not needed for most strictly EGS features)
+		var eosAuthLoginOptions = new LoginOptions()
+		{
+			Credentials = new()
+			{
+				Type = LoginCredentialType.AccountPortal,
+				ExternalType = ExternalCredentialType.Epic,
+				Id = "",
+				Token = "",
+			},
+			ScopeFlags = AuthScopeFlags.BasicProfile,
+		};
+
+		var authInterface = platformInterface.GetAuthInterface();
+		var loginCallbackInfo = await authInterface._LoginAsync(platformInterface,null, eosAuthLoginOptions);
+		Console.WriteLine($"LOGIN COMPLETE, result = {loginCallbackInfo.ResultCode.ToString()}");
+
+		if (loginCallbackInfo.ResultCode != Result.Success)
+		{
+			throw new Exception("failed loginCallbackInfo.  If you have the epic client installed + logged in, the above would have auto popped up an auth webpage and would succeed (on win11 at least)");
+		}
+		else
+		{
+			Console.WriteLine("loginCallbackInfo success" + loginCallbackInfo.LocalUserId.ToString());
+		}
+
+
+		/****************** query user info: translated from: https://dev.epicgames.com/en-US/news/player-authentication-with-epic-account-services-eas *******/
+		//var accountId = loginCallbackInfo.LocalUserId;
+		var userInfoInterface = platformInterface.GetUserInfoInterface();
+
+		var queryUserInfoOptions = new QueryUserInfoOptions()
+		{
+			LocalUserId = loginCallbackInfo.LocalUserId,
+			TargetUserId = loginCallbackInfo.LocalUserId,
+		};
+
+		var queryUserInfoCallbackInfo = await userInfoInterface._QueryUserInfoAsync(platformInterface, queryUserInfoOptions,null);
+		Console.WriteLine($"QueryUserInfo COMPLETE, result = {queryUserInfoCallbackInfo.ResultCode.ToString()}");
+
+		if (queryUserInfoCallbackInfo.ResultCode != Result.Success)
+		{
+			throw new Exception("failed queryUserInfoCallbackInfo");
+		}
+		else
+		{
+			Console.WriteLine("queryUserInfoCallbackInfo User info retrieved");
+		}
+
+		/*****  copyUserInfo: translated from: https://dev.epicgames.com/en-US/news/player-authentication-with-epic-account-services-eas ********/
+		var copyUserInfoOptions = new CopyUserInfoOptions()
+		{
+			LocalUserId = queryUserInfoCallbackInfo.LocalUserId,
+			TargetUserId = queryUserInfoCallbackInfo.TargetUserId
+		};
+		var result = userInfoInterface.CopyUserInfo(ref copyUserInfoOptions, out var userInfoData);
+		Debug.WriteLine($"CopyUserInfo: {result}, DisplayName={userInfoData.Value.DisplayName}");
+
+
+
+
+
+		/**********  "GAME" LOOP *********/
 		var loopCount = 0;
 		while (true)
 		{
@@ -73,6 +147,6 @@ internal class Program
 	private static void Update(PlatformInterface platformInterface, int loopCount)
 	{
 		platformInterface.Tick();
-		Console.WriteLine($"Update Done: loopCount = {loopCount}");
+		Console.Write($" . ");
 	}
 }
---
+++ eos-console-example/zz_Extensions.cs
@@ -0,0 +1,48 @@
+﻿using Epic.OnlineServices.Auth;
+using Epic.OnlineServices.Platform;
+using Epic.OnlineServices.UserInfo;
+
+namespace eos_console_example;
+
+/// <summary>
+/// helper methods to make EOS work with async/await
+/// </summary>
+public static class zz_Extensions
+{
+	public static async Task<LoginCallbackInfo> _LoginAsync(this AuthInterface authInterface, PlatformInterface platformInterface, object clientData, LoginOptions loginOptions)
+	{
+		var tcs = new TaskCompletionSource<LoginCallbackInfo>();
+		authInterface.Login(ref loginOptions, clientData, (ref LoginCallbackInfo callbackInfo)=>tcs.SetResult(callbackInfo));
+
+		Console.WriteLine("_LoginAsync()");
+		while (tcs.Task.IsCompleted is false)
+		{
+			Console.Write(".");
+			await Task.Delay(100);
+			platformInterface.Tick(); //needs to tick for the sdk to do work
+		}
+
+
+		return await tcs.Task;
+	}
+
+	public static async Task<QueryUserInfoCallbackInfo> _QueryUserInfoAsync(this UserInfoInterface userInfoInterface, PlatformInterface platformInterface,  QueryUserInfoOptions queryUserInfoOptions,
+		object clientData)
+	{
+
+		var tcs = new TaskCompletionSource<QueryUserInfoCallbackInfo>();
+
+		userInfoInterface.QueryUserInfo(ref queryUserInfoOptions, clientData, (ref QueryUserInfoCallbackInfo callbackInfo) => tcs.SetResult(callbackInfo));
+
+		Console.WriteLine("_QueryUserInfoAsync()");
+		while (tcs.Task.IsCompleted is false)
+		{
+			Console.Write(".");
+			await Task.Delay(100);
+			platformInterface.Tick(); //needs to tick for the sdk to do work
+		}
+
+
+		return await tcs.Task;
+	}
+}
